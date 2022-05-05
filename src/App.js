4c518d368b8ac1ae5f237f46741a2f2620cf426a
Tahmidd <tahmid.ratul6@gmail.com>
2022-05-05 21:21:23 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Shared/Header/Header';
 import Footer from './Shared/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
 
 
 function App() {
@@ -9,6 +11,12 @@ function App() {
     <div className='page-container'>
       <div className='content-wrap'>
         <Header></Header>
+        <Routes>
+
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/home' element={<Home></Home>}></Route>
+
+        </Routes>
       </div>
       <Footer></Footer>
 
---
+++ src/Pages/Homes/Banner/Banner.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner1 from '../../../images/banner1.jpg'
+import banner2 from '../../../images/banner2.jpg'
+import banner3 from '../../../images/banner3.jpg'
+
+
+const Banner = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+    return (
+        <div className='w-75 container'>
+            <Carousel activeIndex={index} onSelect={handleSelect}>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 img-fluid"
+                        src={banner1}
+                        alt="First slide"
+                    />
+
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 img-fluid"
+                        src={banner2}
+                        alt="Second slide"
+                    />
+
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100 img-fluid"
+                        src={banner3}
+                        alt="Third slide"
+                    />
+
+                </Carousel.Item>
+
+
+
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
